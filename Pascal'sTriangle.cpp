f3ce2947804bb39a3b105cfafd3765139b3471cd
Ayush Singh <81370467+Ayu360@users.noreply.github.com>
2023-01-24 23:56:09 +0530

    Create Pascal'sTriangle.cpp

---
+++ Pascal'sTriangle.cpp
@@ -0,0 +1,20 @@
+//Problem Link: https://leetcode.com/problems/pascals-triangle/?envType=study-plan&id=dynamic-programming-i
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        for (int i = 0; i < numRows; ++i) {
+            result.push_back({});
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    result[i].emplace_back(1);
+                } else {
+                    result[i].emplace_back(result[i - 1][j - 1] +
+                                           result[i - 1][j]);
+                }
+            }
+        }
+        return result;
+    }
+};

2efc8f44cf61fff7ec0f46e20d398bb169869919
Ayush Singh <81370467+Ayu360@users.noreply.github.com>
2023-01-27 20:56:02 +0530

    Create LongestPalindromicSubstring.cpp

---
+++ LongestPalindromicSubstring.cpp
@@ -0,0 +1,39 @@
+//Problem Name: Longest Palindromic Substring
+//Problme Link: https://leetcode.com/problems/longest-palindromic-substring/?envType=study-plan&id=dynamic-programming-i
+
+class Solution {
+public:
+    bool check(string &s, int i, int j)
+{
+    while (i <= j)
+    {
+        if (s[i] != s[j])
+        {
+            return false;
+        }
+        i++, j--;
+    }
+    return true;
+}
+string longestPalindrome(string s)
+{
+    int n = s.length();
+    int max_len = 0;
+    int starting_index = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (check(s, i, j))
+            {
+                if (j - i + 1 > max_len)
+                {
+                    max_len = j - i + 1;
+                    starting_index = i;
+                }
+            }
+        }
+    }
+    return s.substr(starting_index, max_len);
+}
+};
